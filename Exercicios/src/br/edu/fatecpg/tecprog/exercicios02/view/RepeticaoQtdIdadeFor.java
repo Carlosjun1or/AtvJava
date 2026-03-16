2a85d57e269a5c2e4b884300f76fa94fec02d793
Carlos Roberto Dantas Junior <crdantasjr@gmail.com>
2026-03-15 21:33:11 -0300

    Add Exercicios Java project with source & configs
    Add an Eclipse Java project 'Exercicios' with project metadata and settings (.project, .classpath, .settings) and a .gitignore to exclude /bin. Move README into Exercicios/, remove compiled .class files from bin, and add source files under Exercicios/src (MenuOperacoesSwitch, RepeticaoMesWhile, RepeticaoQtdIdadeFor, DezNumInformacoesArray, DezNumNovosValoresArray) along with module-info.java. This commit imports the exercise sources and project configuration while cleaning generated binaries.

---
+++ Exercicios/src/br/edu/fatecpg/tecprog/exercicios02/view/RepeticaoQtdIdadeFor.java
@@ -0,0 +1,20 @@
+package br.edu.fatecpg.tecprog.exercicios02.view;
+
+import java.util.Scanner;
+
+public class RepeticaoQtdIdadeFor {
+	public static void main(String[] args) {
+		int idade[] = new int[10];
+		int c = 0;
+		Scanner scan = new Scanner(System.in);
+		for (int i = 0; i < 10; i++) {
+			System.out.print("Digite a idade: ");
+			idade[i] = scan.nextInt();
+			if (idade[i] > 17) {
+				c += 1;
+			}
+		}
+		System.out.println("\nA quantidade de pessoas com idade maior ou igual a 18 anos: " + c);
+		System.out.println("\nPrograma encerrado.");
+	}
+}
---
+++ Exercicios/src/br/edu/fatecpg/tecprog/exercicios03/view/DezNumNovosValoresArray.java
@@ -0,0 +1,30 @@
+package br.edu.fatecpg.tecprog.exercicios03.view;
+
+import java.util.Scanner;
+
+public class DezNumNovosValoresArray {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int[] valores = new int[10];
+		for (int i = 0; i < 10; i++) {
+			System.out.println("Digite o " + (i + 1) + " número: ");
+			valores[i] = scan.nextInt();
+		}
+		System.out.println("\n Antigos Valores: ");
+		for (int i = 0; i < 10; i++) {
+			System.out.println(valores[i] + " ");
+		}
+		for (int i = 0; i < 10; i++) {
+			if (valores[i] % 2 == 0) {
+				valores[i] = (valores[i] + 1);
+			} else {
+				valores[i] = valores[i] - 1;
+			}
+		}
+		System.out.println("\n Novos Valores: ");
+		for (int i = 0; i < 10; i++) {
+			System.out.println(valores[i] + " ");
+		}
+	}
+}
